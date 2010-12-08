3a64cd48fa74f6fad20d5d69943c846013ffef69
Daniel Westendorf <dwestendorf@ubuntu.(none)>
2010-12-08 10:40:28 -0800

    updated log output to handle long text

--- models.rb
+++ models.rb
@@ -15,7 +15,7 @@ class Log
 
   property :id,             Serial, :key => true
   property :name,           String
-  property :output,         Text, :min => 0, :max => 2 **64 - 1
+  property :output,         Text, :max => 2 **32 - 1
   property :created_at,     DateTime
 
   def find_all_active_errors
